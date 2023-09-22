9eaa57ff58732d4511544d69d19a936ab73d50fc
Joaquin Opazo <ja.opazoy@gmail.com>
2023-09-22 16:07:04 +0200

    Comments

--- TPs/TP-1/cosinus.cpp
+++ TPs/TP-1/cosinus.cpp
@@ -12,3 +12,11 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+
+/*
+Question 3:
+• What types are accepted as parameters for the cosine function ?
+• Which header needs to be included to have the definition of the cosine function ?
+• What argument need to be added to the linking command line to link against the math library ?
+*/
--- TPs/TP-1/hello.cpp
+++ TPs/TP-1/hello.cpp
@@ -7,3 +7,12 @@ int main(int argc, char* argv[]){
     cout << "Guille Pelao" << endl;
     return 0;
 }
+
+/*
+Question 1:
+• What type of file is produced by the above command line ?
+• What does the argument -Wall on the command line correspond to?
+
+Question 2:
+• What type of file is produced by the above command line ?
+*/
