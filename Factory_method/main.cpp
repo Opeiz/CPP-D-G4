e366eb3c9a5d2da161ae50989014aacc6f222290
Guillermo Cossio <guillermo.cossio@imt-atlantique.net>
2023-10-20 15:47:56 +0200

    Pizza v1

--- Factory_method/main.cpp
+++ Factory_method/main.cpp
@@ -8,4 +8,13 @@ int main(){
     PizzaCalezoneKitchen pizzaCalezoneK;
     PizzaVegetarianKitchen PizzaVegetarianK;
     PizzaVeganKitchen PizzaVeganK;
+
+    // Client chooses a type of pizza and the factory cooks it
+    pizzaK = PizzaVegetarianK;
+    Pizza& pizza = pizzaK.cookPizza;
+
+    // Bon appetit!
+    pizza.eatPizza();
+    
+    return 0;
 }
