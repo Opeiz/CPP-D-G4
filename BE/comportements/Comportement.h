da528a61f3f4f37bf297dbd3d5e6246f2656e9e8
Opeiz <ja.opazoy@gmail.com>
2023-11-24 11:05:26 +0100

    update

--- BE/comportements/Comportement.h
+++ BE/comportements/Comportement.h
@@ -1,9 +1,10 @@
 #ifndef _COMP_H_
 #define _COMP_H_
-
+#include "../Bestiole.h"
 class Comportement{
 
-    public :                                                    
+    public : 
+        Bestiole mybestiole ;                                                   
         virtual double get_orientation() = 0 ; 
         virtual double get_vitesse() = 0;
         virtual void set_color() = 0; // TODO: May need to take parameter to change a bestioles color                                    
--- BE/comportements/ComportementKamikaze.cpp
+++ BE/comportements/ComportementKamikaze.cpp
@@ -1,24 +1,27 @@
 #include <cstdlib>
 #include <list>
-
+#include <cmath>
 #include "Bestiole.h"
 #include "UImg.h"
 #include "ComportementKamikaze.h"
 
-
-const T    white[] =    { (T)255,   (T)255, (T)255 };
-const T    black[] =    { (T)0,     (T)0,   (T)0 };
 const T    red[] =      { (T)255,   (T)0,   (T)0 };
-const T    blue[] =     { (T)0,     (T)0,   (T)255 };
-const T    green[] =    { (T)0,     (T)255, (T)0 };
-const T    rose[] =     { (T)255,   (T)193, (T)203 };
-const T    purple[] =   { (T)128,   (T)0,   (T)128 };
-const T    orange[] =   { (T)255,   (T)165, (T)0 };
-
-
+// const T    blue[] =     { (T)0,     (T)0,   (T)255 };
+// const T    green[] =    { (T)0,     (T)255, (T)0 };
+// const T    rose[] =     { (T)255,   (T)193, (T)203 };
+// const T    purple[] =   { (T)128,   (T)0,   (T)128 };
+// const T    orange[] =   { (T)255,   (T)165, (T)0 };
+// const T    white[] =    { (T)255,   (T)255, (T)255 };
+// const T    black[] =    { (T)0,     (T)0,   (T)0 };
 
 double ComportementKamikaze::get_orientation(std::list<Bestiole> perceivedBsts){
-    // TODO
+    //iterate on bestiole to know the nearest 
+    for (nbbestiole , nbbestiole <len(perceivedBsts), nbbestiole++){
+        if ((perceivedBsts[nbbestiole].x)<min){
+            
+        }
+
+    } 
     return 0;
 };
 
@@ -29,4 +32,7 @@ double ComportementKamikaze::get_vitesse(std::list<Bestiole> perceivedBsts){
 
 void ComportementKamikaze::set_color(){
     // TODO
+}
+int ComportementKamikaze::distance(Bestiole bestiole1,Bestiole bestiole2){
+    return sqrt();
 }
